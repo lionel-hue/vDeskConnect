4edc16fb9a3e020cd8095af1df9c76a8c88013f8
lionel sisso <sissolionel@gmail.com>
2026-04-13 12:04:06 +0100

    fixed broken illustrations!, illustrations not responding

--- server/config/cors.php
+++ server/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:5000,http://localhost:5173')),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
