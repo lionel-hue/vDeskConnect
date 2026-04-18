293c46e350aaa5a90c483f7a43b8a007edb3243b
lionel sisso <sissolionel@gmail.com>
2026-04-18 11:52:05 +0100

    added the section id to lectures

--- server/database/migrations/2026_04_18_000002_add_columns_to_lectures_table.php
+++ server/database/migrations/2026_04_18_000002_add_columns_to_lectures_table.php
@@ -10,16 +10,19 @@ return new class extends Migration
     {
         Schema::table('lectures', function (Blueprint $table) {
             if (!Schema::hasColumn('lectures', 'title')) {
-                $table->string('title')->after('section_id')->default('');
+                $table->string('title')->default('');
             }
             if (!Schema::hasColumn('lectures', 'description')) {
-                $table->text('description')->after('title')->nullable();
+                $table->text('description')->nullable();
+            }
+            if (!Schema::hasColumn('lectures', 'section_id')) {
+                $table->foreignId('section_id')->nullable()->constrained()->onDelete('set null');
             }
             if (!Schema::hasColumn('lectures', 'is_online')) {
-                $table->boolean('is_online')->default(false)->after('status');
+                $table->boolean('is_online')->default(false);
             }
             if (!Schema::hasColumn('lectures', 'created_by')) {
-                $table->foreignId('created_by')->nullable()->after('meeting_link')->constrained('users')->onDelete('set null');
+                $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
             }
         });
     }
