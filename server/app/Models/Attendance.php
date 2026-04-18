71b8edfc597e4ad4bbd0f8ccd50b5d93ee8ed57a
lionel sisso <sissolionel@gmail.com>
2026-04-18 11:34:29 +0100

    attendance model

---
+++ server/app/Models/Attendance.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'school_id',
+        'lecture_id',
+        'student_id',
+        'status',
+        'checked_at',
+    ];
+
+    protected $casts = [
+        'checked_at' => 'datetime',
+    ];
+
+    public function lecture(): BelongsTo
+    {
+        return $this->belongsTo(Lecture::class);
+    }
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+}
