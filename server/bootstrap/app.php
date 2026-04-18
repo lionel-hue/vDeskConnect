e79ea75575c14a5ed733693b3736b2a0d214f36b
lionel sisso <sissolionel@gmail.com>
2026-04-18 17:30:17 +0100

    added mem limit deneutraliser -> php.ini custom 2

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -4,6 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Increase upload limits early
+ini_set('upload_max_filesize', '200M');
+ini_set('post_max_size', '200M');
+ini_set('memory_limit', '256M');
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
